501c2186415924604b1e4be1e2007e47fa06f0a6
CodeCrafter-droid <singh.sahasofficial@gmail.com>
2025-01-25 21:54:11 +0530

    Add files via upload
    Initial upload of project files.

---
+++ script.js
@@ -0,0 +1,388 @@
+const data = {
+    "products": [
+        {
+            "mens": [
+                {
+                    "id": 1,
+                    "title": "Rainbow logo tee",
+                    "description": "If your entire wardrobe is black and at least one person in your life has told you to wear more color, this shirt is the compromise you've been waiting for.",
+                    "price": 549,
+                    "discountPercentage": 12.96,
+                    "rating": 4.69,
+                    "stock": 94,
+                    "quantity": 2,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/Figma-Store-1096_1000x.jpg?v=1636599836",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-1039_1000x.jpg?v=1636599836",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-1108_1000x.jpg?v=1636599836",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store---Product-4381_1000x.jpg?v=1636323317"
+                    ]
+                },
+                {
+                    "id": 2,
+                    "title": "Tidy up tee",
+                    "description": "If only there was a Tidy Up button for our lives…sigh.",
+                    "price": 899,
+                    "discountPercentage": 17.94,
+                    "rating": 4.44,
+                    "stock": 34,
+                    "quantity": 1,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/figma-store_tidy-up-tee_01_1000x.jpg?v=1670516715",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/figma-store_tidy-up-tee_01_1000x.jpg?v=1670516715",
+                        "https://store.figma.com/cdn/shop/products/figma-store_tidy-up-tee_01_1000x.jpg?v=1670516715",
+                        "https://store.figma.com/cdn/shop/products/figma-store_tidy-up-tee_01_1000x.jpg?v=1670516715"
+                    ]
+                },
+                {
+                    "id": 3,
+                    "title": "Pixel perfect dark mode tee",
+                    "description": "This tee is in dark mode, just like your design soul. Bask in the darkness and blend into your UI—if only for a moment—before the reflective logo picks up the light of a computer screen.",
+                    "price": 1499,
+                    "discountPercentage": 4.15,
+                    "rating": 4.25,
+                    "stock": 50,
+                    "quantity": 4,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/IMG_9207_1000x.png?v=1651177999",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/IMG_9221_1000x.jpg?v=1651178100",
+                        "https://store.figma.com/cdn/shop/products/IMG_9221_1000x.jpg?v=1651178100",
+                        "https://store.figma.com/cdn/shop/products/IMG_9221_1000x.jpg?v=1651178100",
+                        "https://store.figma.com/cdn/shop/products/IMG_0060_1000x.jpg?v=1652132420"
+                    ]
+                }
+            ],
+            "womens": [
+                {
+                    "id": 4,
+                    "title": "Framework tee",
+                    "description": "Work that frame of yours in a tee that screams “I’ve got pixels and I know how to use ‘em.”",
+                    "price": 1249,
+                    "discountPercentage": 15.46,
+                    "rating": 4.09,
+                    "stock": 36,
+                    "quantity": 5,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/figma-store_framework-tee_01_1000x.jpg?v=1670515454",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/figma-store_framework-tee_01_1000x.jpg?v=1670515454",
+                        "https://store.figma.com/cdn/shop/products/figma-store_framework-tee_03_1000x.jpg?v=1670597451",
+                        "https://store.figma.com/cdn/shop/products/figma-store_framework-tee_04_1000x.jpg?v=1670597451",
+                        "https://store.figma.com/cdn/shop/products/figma-store_framework-tee_05_1000x.jpg?v=1670597451"
+                    ]
+                },
+                {
+                    "id": 5,
+                    "title": "Dress code hat",
+                    "description": "Are you casting a spell or is that just hex code? Yes.",
+                    "price": 280,
+                    "discountPercentage": 17.91,
+                    "rating": 4.3,
+                    "stock": 123,
+                    "quantity": 5,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_600x.png?v=1677707527",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_1000x.png?v=1677707527",
+                        "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_1000x.png?v=1677707527",
+                        "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_1000x.png?v=1677707527",
+                        "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_1000x.png?v=1677707527",
+                        "https://store.figma.com/cdn/shop/products/figma-store_dress-code-hat_01_1000x.png?v=1677707527"
+                    ]
+                },
+                {
+                    "id": 6,
+                    "title": "Figma wordmark tee",
+                    "description": "We’ve heard that a plain tee says something by saying nothing. We like to think that this one says… Figma.",
+                    "price": 1499,
+                    "discountPercentage": 10.23,
+                    "rating": 4.43,
+                    "stock": 68,
+                    "quantity": 1,
+                    "brand": "",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/Figma-Store-3814_1000x.jpg?v=1636599369",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3804_1000x.jpg?v=1636599369",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3917_1000x.jpg?v=1636599369",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3917_1000x.jpg?v=1636599369"
+                    ]
+                },
+                {
+                    "id": 7,
+                    "title": "Infinix INBOOK",
+                    "description": "Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+                    "price": 1099,
+                    "discountPercentage": 11.83,
+                    "rating": 4.54,
+                    "stock": 96,
+                    "quantity": 2,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/Figma-Store-2327_600x.jpg?v=1636599425",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-2327_600x.jpg?v=1636599425",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-2327_600x.jpg?v=1636599425",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-2327_600x.jpg?v=1636599425",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-2327_600x.jpg?v=1636599425"
+                    ]
+                }
+            ],
+            "accessories": [
+                {
+                    "id": 8,
+                    "title": "Toolkit mug",
+                    "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+                    "price": 499,
+                    "discountPercentage": 10.58,
+                    "rating": 4.09,
+                    "stock": 32,
+                    "quantity": 3,
+                    "brand": "Figma",
+                    "category": "apparels",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/figma-store_tool-kit-mug_01_1000x.jpg?v=1670520016",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/figma-store_tool-kit-mug_02_1000x.jpg?v=1670520015",
+                        "https://store.figma.com/cdn/shop/products/figma-store_tool-kit-mug_02_1000x.jpg?v=1670520015",
+                        "https://store.figma.com/cdn/shop/products/figma-store_tool-kit-mug_02_1000x.jpg?v=1670520015",
+                        "https://store.figma.com/cdn/shop/products/figma-store_tool-kit-mug_02_1000x.jpg?v=1670520015"
+                    ]
+                },
+                {
+                    "id": 9,
+                    "title": "Smiley Face Pin",
+                    "description": "What we imagine you look like every time you open a Figma file.",
+                    "price": 1749,
+                    "discountPercentage": 11.02,
+                    "rating": 4.57,
+                    "stock": 83,
+                    "quantity": 5,
+                    "brand": "Apple",
+                    "category": "laptops",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/Figma-Store-3504_1000x.jpg?v=1636599916",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3550_1000x.jpg?v=1636599916",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3579_1000x.jpg?v=1636599916",
+                        "https://store.figma.com/cdn/shop/products/Figma-Store-3579_1000x.jpg?v=1636599916"
+                    ]
+                },
+                {
+                    "id": 10,
+                    "title": "Cursor tote bag",
+                    "description": "Totes are just a mobile version of that one really full drawer of random stuff we all have at home. We celebrate that with this canvas of possibilities.",
+                    "price": 1099,
+                    "discountPercentage": 6.18,
+                    "rating": 4.43,
+                    "stock": 89,
+                    "quantity": 5,
+                    "brand": "HP Pavilion",
+                    "category": "laptops",
+                    "thumbnail": "https://store.figma.com/cdn/shop/products/FigmaStore_Tote-25_1000x.jpg?v=1657055226",
+                    "images": [
+                        "https://store.figma.com/cdn/shop/products/FigmaStore_Tote-25_1000x.jpg?v=1657055226",
+                        "https://store.figma.com/cdn/shop/products/FigmaStore_Tote-25_1000x.jpg?v=1657055226",
+                        "https://store.figma.com/cdn/shop/products/FigmaStore_Tote-25_1000x.jpg?v=1657055226"
+                    ]
+                }
+            ]
+        }
+    ]
+};
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX HOME PAGE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+document.addEventListener('click', function(event) {
+    const selectMenu = document.getElementById('selectMenu');
+    
+    // Check if the click was on the select menu
+    if (selectMenu.contains(event.target)) {
+        selectMenu.classList.add('active');
+    } else {
+        // Remove the active class if clicking outside the select menu
+        selectMenu.classList.remove('active');
+    }
+});
+
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        navbar.style.top = "-91px"; // Adjust based on navbar height
+    } else {
+        // Scrolling up
+        navbar.style.top = "0";
+    }
+       
+    lastScrollTop = scrollTop;
+
+});
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX CATEGORY PAGE XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+function getUrlParameter(name) {
+    const regex = new RegExp(`[?&]${name}=([^&#]*)`);
+    const results = regex.exec(window.location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function displayCategories() {
+    // No need to implement, handled in index.html
+}
+
+function displayProducts() {
+    const category = getUrlParameter('category');
+    const products = data.products[0][category] || [];
+    const container = document.getElementById('products-container');
+    document.getElementById('category-title').textContent = category.charAt(0).toUpperCase() + category.slice(1);
+    container.innerHTML = products.map(product => `
+        <div class="product-card" onclick="location.href='product.html?id=${product.id}&category=${category}'">
+            <img src="${product.thumbnail}" alt="${product.title}">
+            <div class="infu">
+            <h2>${product.title}</h2>
+            <p style="font-family: Arial, sans-serif">${product.price} USD</p>
+            </div>
+        </div>
+    `).join('');
+ 
+}
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX PRODUCT PAGE xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+let cartCount = 0; //Default cart count
+let selectedQuantity = 1; //Default selected quantity
+
+function updateCartCount() {
+    document.getElementById('cart-count').textContent = cartCount;
+    toggleCartCountButton()
+}
+function toggleCartCountButton() {
+    const cartLink = document.getElementById('cart-link');
+    if (cartCount > 0) {
+        cartLink.classList.remove('disabled');
+    } else {
+        cartLink.classList.add('disabled');
+    }
+}
+
+function updateQuantityDisplay() {
+    document.getElementById('quantity-display').textContent = selectedQuantity;
+}
+
+function increaseQuantity(stock) {
+    if (selectedQuantity < stock) {
+        selectedQuantity++;
+        updateQuantityDisplay();
+    } else {
+        alert("Cannot add more than available stock.");
+    }
+}
+
+function decreaseQuantity() {
+    if (selectedQuantity > 1) {
+        selectedQuantity--;
+        updateQuantityDisplay();
+    }
+}
+
+function addToCart(stock) {
+    cartCount += selectedQuantity;
+    updateCartCount();
+    alert(`${selectedQuantity} item(s) added to cart!`);
+    selectedQuantity = 1; // Reset the selected quantity after adding to cart
+    updateQuantityDisplay();
+}
+
+function updateMainImage(src) {
+    document.getElementById('main-product-image').src = src;
+}
+
+
+function updateSize(size) {
+    selectedSize = size;
+
+    // Update the active size button styling
+    const sizeButtons = document.querySelectorAll('.size-radio');
+    sizeButtons.forEach(button => {
+        button.classList.remove('active-size');
+    });
+
+    const activeButton = document.querySelector(`input[value="${size}"]`).parentElement;
+    activeButton.classList.add('active-size');
+}
+
+function displayProductDetails() {
+    const id = getUrlParameter('id');
+    const category = getUrlParameter('category');
+    const product = (data.products[0][category] || []).find(p => p.id == id);
+    if (!product) return;
+
+    const container = document.getElementById('product-details');
+    container.innerHTML = `
+    <div class="pico">
+        <div class="thumbnail-images">
+            ${product.images.map(image => `
+                <img src="${image}" alt="${product.title}" class="thumbnail-image" onclick="updateMainImage('${image}')">
+            `).join('')}
+        </div>
+        <div class="bpic">
+        <img id="main-product-image" src="${product.images[0]}" alt="${product.title}">
+        </div>
+        </div>
+        <div class="s1">
+        <p style="background-color: #ffc700">Discount: ${product.discountPercentage}%</p>
+        <p>Rating: ${product.rating}</p>
+        </div>
+        <h1>${product.title}</h1> 
+        <div class="ms"> 
+        <div class="radio-group">
+            ${['XS', 'S', 'M', 'L', 'XL', '2XL'].map(size => `
+                <div class="size-radio ${size === 'XS' ? 'active-size' : ''}">
+                    <input type="radio" id="size-${size.toLowerCase()}" name="size" value="${size}" ${size === 'XS' ? 'checked' : ''} onclick="updateSize('${size}')">
+                    <label for="size-${size.toLowerCase()}" class="label">${size}</label>
+                </div>
+            `).join('')}
+        </div> 
+        <div class="s2">         
+        <div class="quantity-control">
+            <button onclick="decreaseQuantity()">
+            <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="24" cy="24" r="23" stroke="black" stroke-width="2"></circle>
+          <path d="M16 24H32" stroke="black" stroke-width="2"></path>
+        </svg>
+        </button>
+            <span id="quantity-display">1</span>
+            <button onclick="increaseQuantity(${product.stock})">
+            <svg  width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="24" cy="24" r="23" stroke="black" stroke-width="2"></circle>
+          <path d="M16 24H32" stroke="black" stroke-width="2"></path>
+          <path d="M24 32V16" stroke="black" stroke-width="2"></path>
+        </svg>
+        </button> 
+        </div> 
+         <p>Stock: ${product.stock}</p>  
+        </div>
+        <button  class="cart-button" onclick="addToCart(${product.stock})">ADD ${product.price} USD</button>
+        </div> 
+        <p class="discrip">${product.description}</p>
+    `;
+}
+
+updateCartCount();
+
+if (document.getElementById('products-container')) {
+    displayProducts();
+} else if (document.getElementById('product-details')) {
+    displayProductDetails();
+}
+
+
